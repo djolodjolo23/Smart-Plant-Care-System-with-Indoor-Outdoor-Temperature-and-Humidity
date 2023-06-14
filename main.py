9f39d06afd81f3d12b6fd3ecf92b40d87ee4825b
Djordje Dimitrov <110050027+djolodjolo23@users.noreply.github.com>
2023-06-14 15:47:41 +0200

    Uploading data to Adafruit works fine.

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import time
 from machine import Pin, ADC
+from mqtt import MQTTClient
+
 
 fully_dry = 44490  # 0% wet
 fully_wet = 16500  # 100% wet
+
+ADAFRUIT_IO_USERNAME = "djolodjolo"
+ADAFRUIT_IO_KEY = "aio_MFoi10Z8EuG91Jfuv0Gr3cnnRQAQ"
+
+mqtt_client = MQTTClient("", "io.adafruit.com", user=ADAFRUIT_IO_USERNAME, password=ADAFRUIT_IO_KEY)
+mqtt_client.connect()
+
+
 
 def get_soil_moisture_percentage(adc_value):
     adc_range = fully_dry - fully_wet
@@ -15,8 +25,9 @@ def get_capacitator_measurements():
     while True:
         adc = soil_adc.read_u16()
         moisture_perc = get_soil_moisture_percentage(adc)
-        print("MOISTURE PERCENTAGE IS -> " + str(round(moisture_perc, 1)) + "%")
-        time.sleep(1)
+        mqtt_client.publish(topic="djolodjolo/feeds/moisture-sensor", msg=str(moisture_perc))
+        print('Sent to Adafruit IO: ', moisture_perc)
+        time.sleep(5)
 
 
 get_capacitator_measurements()
