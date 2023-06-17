49f8e491b723b9f1cd1531a822995b5e94c79ec1
Djordje Dimitrov <110050027+djolodjolo23@users.noreply.github.com>
2023-06-17 12:19:11 +0200

    Added a webhook to inform me when the sensor moisture is bellow 10%

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import time
 from machine import Pin, ADC
 from mqtt import MQTTClient
+import urequests
+import json
 
 
 fully_dry = 44490  # 0% wet
 fully_wet = 16500  # 100% wet
+webhook_url = "https://discord.com/api/webhooks/1118471807377346592/SyTomR8MQRTJVds3mfL_pIfAIQFSEB_lgiLI97WHnJD9TrQAQhRMC5wPau5BNCevGR6G"
 
 ADAFRUIT_IO_USERNAME = "djolodjolo"
 ADAFRUIT_IO_KEY = "aio_MFoi10Z8EuG91Jfuv0Gr3cnnRQAQ"
 
 mqtt_client = MQTTClient("", "io.adafruit.com", user=ADAFRUIT_IO_USERNAME, password=ADAFRUIT_IO_KEY)
 mqtt_client.connect()
+
+def send_to_discord(value, sensor_name):
+    if value <= 10:
+        payload = {
+            "content": f"WARNING!: Soil moisture percentage on {sensor_name} is: {value}%"
+        }
+        headers = {
+            "Content-Type": "application/json"
+        }
+        response = urequests.post(webhook_url, data=json.dumps(payload), headers=headers)
+        if response.status_code == 204:
+            print("Sent to Discord:", value)
+        else:
+            print("Failed to send to Discord:", response.text)
 
 
 
@@ -32,10 +49,12 @@ def get_capacitator_measurements():
         if not first_iteration:
             mqtt_client.publish(topic="djolodjolo/feeds/1st_moisture_sensor", msg=str(moisture_perc1))
             print('Sent to 1st moisture sensor feed : ', moisture_perc1)
+            send_to_discord(moisture_perc1, "1st sensor")
             mqtt_client.publish(topic="djolodjolo/feeds/2nd_moisture_sensor", msg=str(moisture_perc2))
             print('Sent to 2nd moisture sensor feed : ', moisture_perc2)
+            send_to_discord(moisture_perc2, "2nd sensor")
         first_iteration = False
-        time.sleep(10)
+        time.sleep(60)
 
 
 get_capacitator_measurements()
